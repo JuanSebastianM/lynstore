1b0b2857afaaef0f9d64c7d5e7e39b80ba2233e1
Juan Sebastian Mendoza Perez <cebo_052000@hotmail.com>
2021-11-18 20:25:02 -0500

    Change scroll behavior. Change background color to match that of Navbar.js

--- src/components/SideMenu.js
+++ src/components/SideMenu.js
@@ -1,45 +1,59 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import styled from 'styled-components'
-
-const Container = styled.div`
-  /* background-color: #d4e0de; */
-  /* background: linear-gradient(0deg, rgba(0, 0, 0) 0%, rgba(115, 85, 86) 100%); */
-  border-top: 2px solid #000;
-  position: absolute;
-  display: flex;
-  left: 0;
-  text-align: center;
-  justify-content: center;
-  padding-top: 100px;
+const OuterContainer = styled.div`
   width: 100%;
-  min-height: 100vh;
+  position: absolute;
+  top: 80px;
+  left: 0;
+  display: flex;
   transform: translateX(-100%);
   transition: transform 500ms ease;
+  min-height: 100vh;
+`
+const Container = styled.div`
+  box-shadow: 8px 1px 20px 0px rgba(0, 0, 0, 0.6);
+  padding-top: 100px;
+  width: 60%;
+  min-height: 100vh;
+`
+const SideDiv = styled.div`
+  background-color: rgba(0, 0, 0, 0.6);
+  width: 40%;
+  min-height: 100vh;
 `
 const Wrapper = styled.ul``
 const List = styled.li`
   margin-bottom: 20px;
 `
 const Link = styled.a`
-  color: white;
+  color: #000;
 `
-export const SideMenu = ({ menuRef }) => {
+export const SideMenu = ({ menuRef, contRef, btnRef }) => {
+  const hideSideMenu = () => {
+    if (contRef.current.classList.contains('active')) {
+      contRef.current.classList.remove('active')
+      btnRef.current.classList.remove('btn-active')
+    }
+  }
   return (
-    <Container ref={menuRef}>
-      <Wrapper>
-        <List>
-          <Link href='/'>Inicio</Link>
-        </List>
-        <List>
-          <Link href='/'>¿Quiénes Somos?</Link>
-        </List>
-        <List>
-          <Link href='/'>¿Quiénes Somos?</Link>
-        </List>
-        <List>
-          <Link href='/'>¿Quiénes Somos?</Link>
-        </List>
-      </Wrapper>
-    </Container>
+    <OuterContainer ref={contRef}>
+      <Container className='navbar-office' ref={menuRef}>
+        <Wrapper>
+          <List>
+            <Link href='/'>Inicio</Link>
+          </List>
+          <List>
+            <Link href='/'>¿Quiénes Somos?</Link>
+          </List>
+          <List>
+            <Link href='/'>¿Quiénes Somos?</Link>
+          </List>
+          <List>
+            <Link href='/'>¿Quiénes Somos?</Link>
+          </List>
+        </Wrapper>
+      </Container>
+      <SideDiv onClick={hideSideMenu}></SideDiv>
+    </OuterContainer>
   )
 }
