1d19fe8b99ffdb5a585dec33f311392473a73a61
Juan Sebastian Mendoza Perez <cebo_052000@hotmail.com>
2021-11-24 20:44:08 -0500

    Remove commented code

--- src/UI/Hero/Hero.js
+++ src/UI/Hero/Hero.js
@@ -46,20 +46,6 @@ const Hero = () => {
     <HeroContainer>
       <TextWrapper>
         <HeroText />
-        {/* <h1
-          css={`
-            text-align: center;
-            margin-bottom: 30px;
-          `}
-        >
-          Mereces lo que sueñas
-        </h1>
-        <p>
-          Vistete como quieras, sé quien quieras, estudia lo que quieras, viaja
-          a donde quieras.
-        </p>
-        <p>Sé feliz como TÚ QUIERAS.</p>
-        <p>Sé feliz con LynStore.</p> */}
         <HeroLinks />
       </TextWrapper>
     </HeroContainer>
