a57fcc4887ca0eeb5ebcea1e9d455b9623a91734
Juan Sebastian Mendoza Perez <cebo_052000@hotmail.com>
2021-11-24 20:14:25 -0500

    Add a component that contains all the content imported from the UI folder

---
+++ src/components/Content.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import styled from 'styled-components/macro'
+
+import Hero from '../UI/Hero/Hero'
+import About from '../UI/About/About'
+
+const Wrapper = styled.main`
+  width: 100%;
+  min-height: 100vh;
+  background-color: #d4e0de;
+`
+
+export const Content = () => {
+  return (
+    <>
+      <Wrapper>
+        <Hero />
+        <About />
+      </Wrapper>
+    </>
+  )
+}
