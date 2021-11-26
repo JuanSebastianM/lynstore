6afaf52d7129a8d9297cee7c6086b43ef88b26c3
Juan Sebastian Mendoza Perez <cebo_052000@hotmail.com>
2021-11-25 20:28:06 -0500

    Add a new styled div to wrap the links in a flex mode

--- src/UI/Hero/HeroLinks.js
+++ src/UI/Hero/HeroLinks.js
@@ -1,32 +1,28 @@
 import styled from 'styled-components'
 
 const LinkBtn = styled.a`
-  margin-right: 15px;
+  display: block;
   text-align: center;
   padding: 5px 10px;
-  display: inline-block;
   width: 120px;
   border-radius: 10px;
   background-color: #000;
   color: #d4e0de;
-  font-size: 1.2rem;
   cursor: pointer;
   transform: scale(1);
   transition: all 500ms ease;
   @media screen and (min-width: 1023px) {
     width: 160px;
-    margin-right: 20px;
   }
   &:hover {
     color: #000;
-    background-color: #d4e0de;
+    background-color: transparent;
   }
   &:active {
     transform: scale(0.8);
   }
 `
 const AboutUsBtn = styled(LinkBtn)`
-  margin: 0;
   background-color: #d4e0de;
   color: #000;
   &:hover {
@@ -34,12 +30,18 @@ const AboutUsBtn = styled(LinkBtn)`
     background-color: #000;
   }
 `
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+`
 
 const HeroLinks = () => {
   return (
     <>
-      <LinkBtn>Visita nuestra tienda</LinkBtn>
-      <AboutUsBtn>Conoce más sobre nosotros</AboutUsBtn>
+      <Wrapper>
+        <LinkBtn>Visita nuestra tienda</LinkBtn>
+        <AboutUsBtn>Conoce más sobre nosotros</AboutUsBtn>
+      </Wrapper>
     </>
   )
 }
