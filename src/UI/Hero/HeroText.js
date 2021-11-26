8fbc5ecdb59df2a6f72ef855a0624134b982357a
Juan Sebastian Mendoza Perez <cebo_052000@hotmail.com>
2021-11-25 20:22:59 -0500

    Add a styled span, and remove custom css from the h2 tag as it is a global style now

--- src/UI/Hero/HeroText.js
+++ src/UI/Hero/HeroText.js
@@ -1,22 +1,32 @@
 import styled from 'styled-components/macro'
 
+const StyledText = styled.span`
+  color: #d4c3d0;
+  background-image: linear-gradient(120deg, #000 0%, #000 100%);
+  background-repeat: no-repeat;
+  background-size: 100% 0.2em;
+  background-position: 0% 88%;
+  transition: background-size 500ms ease;
+  &:hover {
+    background-size: 100% 88%;
+  }
+  border-radius: 50%;
+`
+
 const HeroText = () => {
   return (
     <>
-      <h1
-        css={`
-          text-align: center;
-          margin-bottom: 30px;
-        `}
-      >
-        Mereces lo que sueñas
-      </h1>
+      <header>
+        <h2>Mereces lo que sueñas</h2>
+      </header>
       <p>
         Vistete como quieras, sé quien quieras, estudia lo que quieras, viaja a
         donde quieras.
       </p>
       <p>Sé feliz como TÚ QUIERAS.</p>
-      <p>Sé feliz con LynStore.</p>
+      <p>
+        Sé feliz con <StyledText>LynStore</StyledText>.
+      </p>
     </>
   )
 }
