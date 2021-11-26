93497da79a285c520c4282ae90167284ee40217b
Juan Sebastian Mendoza Perez <cebo_052000@hotmail.com>
2021-11-25 20:29:30 -0500

    Improve cards behavior both on mobile and desktop. Change their position on desktop, as well

--- src/UI/About/AboutImage.js
+++ src/UI/About/AboutImage.js
@@ -27,6 +27,9 @@ const breezeTwo = keyframes`
 `
 const ImageContainer = styled.div`
   min-height: 100vh;
+  @media screen and (min-width: 1023px) {
+    position: relative;
+  }
 `
 const SiteImage = styled.img`
   object-fit: contain;
@@ -37,6 +40,14 @@ const ImagesContainer = styled.div`
   display: flex;
   height: 140px;
   justify-content: center;
+  @media screen and (min-width: 1023px) {
+    position: absolute;
+    top: 0;
+    right: -60px;
+    flex-direction: column;
+    height: 100%;
+    justify-content: space-evenly;
+  }
 `
 const ImageOneContainer = styled.div`
   box-shadow: 4px 4px 5px -4px #000;
@@ -46,8 +57,13 @@ const ImageOneContainer = styled.div`
   height: 120px;
   align-self: flex-end;
   transition: all 500ms ease;
-  transform-origin: center -20px;
+  transform-origin: center -30px;
   transform: rotate(15deg);
+  @media screen and (min-width: 1023px) {
+    width: 180px;
+    height: 200px;
+    transform-origin: center -40px;
+  }
   &:hover {
     animation: ${breezeOne} 2.5s infinite;
   }
@@ -58,14 +74,24 @@ const ImageOneContainer = styled.div`
     content: '';
     width: 5px;
     height: 30px;
+    border-top: 5px solid #000;
     border-top-left-radius: 30%;
     border-top-right-radius: 30%;
     background-color: #fff;
+    @media screen and (min-width: 1023px) {
+      top: -40px;
+      left: 85px;
+      width: 10px;
+      height: 40px;
+    }
   }
 `
 const ImageCardOne = styled(SiteImage)`
   transform: rotate(10deg);
   filter: drop-shadow(0px 1px 6px #fff);
+  @media screen and (min-width: 1023px) {
+    width: 190px;
+  }
 `
 const ImageTwoContainer = styled(ImageOneContainer)`
   transform: rotate(-15deg);
