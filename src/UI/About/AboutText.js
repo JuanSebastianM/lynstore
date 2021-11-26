a4d99d48cec54bf1bbb038b51f39b07502256c82
Juan Sebastian Mendoza Perez <cebo_052000@hotmail.com>
2021-11-25 20:26:32 -0500

    Add a new styled div element and updated the content

--- src/UI/About/AboutText.js
+++ src/UI/About/AboutText.js
@@ -1,15 +1,26 @@
+import styled from 'styled-components/macro'
+
+const Container = styled.div`
+  margin: 0 auto;
+  max-width: 400px;
+  width: 80%;
+  @media screen and (min-width: 1023px) {
+    width: 50%;
+  }
+`
+
 const AboutText = () => {
   return (
-    <div>
+    <Container>
       <header>
         <h2>¿Quiénes sómos?</h2>
       </header>
       <p>
-        Somos fabricantes de accesorios para todos los gustos y edades 🌸🙌✨
+        + Somos fabricantes de accesorios para todos los gustos y edades 🌸🙌✨
       </p>
-      <p>Vendemos al detal y por mayor</p>
-      <p>Hacemos envíos a toda Colombia.</p>
-    </div>
+      <p>+ Vendemos al detal y por mayor.</p>
+      <p>+ Hacemos envíos a toda Colombia.</p>
+    </Container>
   )
 }
 export default AboutText
