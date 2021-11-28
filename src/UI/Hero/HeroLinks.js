af87a112f83317b32fd446c9ecae140dd675c230
Juan Sebastian Mendoza Perez <cebo_052000@hotmail.com>
2021-11-27 20:17:37 -0500

    Improve style of links and change the texts

--- src/UI/Hero/HeroLinks.js
+++ src/UI/Hero/HeroLinks.js
@@ -4,19 +4,20 @@ const LinkBtn = styled.a`
   display: block;
   text-align: center;
   padding: 5px 10px;
-  width: 120px;
   border-radius: 10px;
   background-color: #000;
   color: #d4e0de;
   cursor: pointer;
+  border: 2px solid transparent;
   transform: scale(1);
   transition: all 500ms ease;
   @media screen and (min-width: 1023px) {
-    width: 160px;
+    width: 180px;
   }
   &:hover {
     color: #000;
     background-color: transparent;
+    border: 2px solid #000;
   }
   &:active {
     transform: scale(0.8);
@@ -27,7 +28,6 @@ const AboutUsBtn = styled(LinkBtn)`
   color: #000;
   &:hover {
     color: #d4e0de;
-    background-color: #000;
   }
 `
 const Wrapper = styled.div`
@@ -39,8 +39,8 @@ const HeroLinks = () => {
   return (
     <>
       <Wrapper>
-        <LinkBtn>Visita nuestra tienda</LinkBtn>
-        <AboutUsBtn>Conoce más sobre nosotros</AboutUsBtn>
+        <LinkBtn>Comprar</LinkBtn>
+        <AboutUsBtn>Conocer más</AboutUsBtn>
       </Wrapper>
     </>
   )
