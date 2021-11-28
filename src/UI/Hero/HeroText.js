45b818336c01955fa8898e436e2561063d70c602
Juan Sebastian Mendoza Perez <cebo_052000@hotmail.com>
2021-11-27 20:16:03 -0500

    Add new styled element

--- src/UI/Hero/HeroText.js
+++ src/UI/Hero/HeroText.js
@@ -1,7 +1,6 @@
 import styled from 'styled-components/macro'
 
 const StyledText = styled.span`
-  color: #d4c3d0;
   background-image: linear-gradient(120deg, #000 0%, #000 100%);
   background-repeat: no-repeat;
   background-size: 100% 0.2em;
@@ -12,6 +11,9 @@ const StyledText = styled.span`
   }
   border-radius: 50%;
 `
+const Paragraph = styled.p`
+  margin-bottom: 15px;
+`
 
 const HeroText = () => {
   return (
@@ -19,14 +21,14 @@ const HeroText = () => {
       <header>
         <h2>Mereces lo que sueñas</h2>
       </header>
-      <p>
+      <Paragraph>
         Vistete como quieras, sé quien quieras, estudia lo que quieras, viaja a
         donde quieras.
-      </p>
-      <p>Sé feliz como TÚ QUIERAS.</p>
-      <p>
+      </Paragraph>
+      <Paragraph>Sé feliz como TÚ QUIERAS.</Paragraph>
+      <Paragraph>
         Sé feliz con <StyledText>LynStore</StyledText>.
-      </p>
+      </Paragraph>
     </>
   )
 }
