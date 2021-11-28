8c9f13725c0b511f10e0e485526f59e5726204a1
Juan Sebastian Mendoza Perez <cebo_052000@hotmail.com>
2021-11-27 20:03:10 -0500

    Update styles and improve semantic

--- src/UI/Hero/Hero.js
+++ src/UI/Hero/Hero.js
@@ -2,18 +2,14 @@ import styled from 'styled-components'
 import HeroLinks from './HeroLinks'
 import HeroText from './HeroText'
 
-const HeroContainer = styled.div`
+const HeroContainer = styled.section`
   width: 100%;
   min-height: 100vh;
   display: flex;
   justify-content: center;
   align-items: center;
   padding-top: 60px;
-  background-image: linear-gradient(
-      0deg,
-      rgba(0, 0, 0, 0.5),
-      rgba(0, 0, 0, 0.3)
-    ),
+  background-image: linear-gradient(0deg, rgb(0 0 0 / 45%), rgb(0 0 0 / 42%)),
     url('https://images.unsplash.com/photo-1583292650898-7d22cd27ca6f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80');
   background-position: top;
   background-size: cover;
@@ -23,22 +19,20 @@ const HeroContainer = styled.div`
   @media screen and (min-width: 1023px) {
     justify-content: flex-start;
     padding-left: 80px;
-    background-image: linear-gradient(
-        0deg,
-        rgba(0, 0, 0, 0.5),
-        rgba(0, 0, 0, 0.3)
-      ),
-      url('https://images.unsplash.com/photo-1583292650898-7d22cd27ca6f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80');
     background-position: top;
     color: #000;
   }
 `
 const TextWrapper = styled.div`
   max-width: 400px;
-  width: 80%;
+  padding: 0 25px;
   letter-spacing: 1.5px;
+  background-color: rgba(0, 0, 0, 0.25);
   color: #fff;
-  text-shadow: 2px 2px 6px #000;
+  @media screen and (min-width: 1023px) {
+    padding: 0;
+    background-color: transparent;
+  }
 `
 
 const Hero = () => {
