bee4f139cb826a8e07716639f849de0832bb58ab
Juan Sebastian Mendoza Perez <cebo_052000@hotmail.com>
2021-11-28 21:29:36 -0500

    Edit style of the span element

--- src/UI/Hero/HeroText.js
+++ src/UI/Hero/HeroText.js
@@ -1,12 +1,15 @@
 import styled from 'styled-components/macro'
 
 const StyledText = styled.span`
+color: #d4e0d3;
+text-shadow: 2px 1px 0px #54adb5;
   background-image: linear-gradient(120deg, #000 0%, #000 100%);
   background-repeat: no-repeat;
   background-size: 100% 0.2em;
   background-position: 0% 88%;
-  transition: background-size 500ms ease;
+  transition: all 500ms ease;
   &:hover {
+    text-shadow: none;
     background-size: 100% 88%;
   }
   border-radius: 50%;
