09d6f744b27c9fb4fa68b475b825fa8d23a28295
Juan Sebastian Mendoza Perez <cebo_052000@hotmail.com>
2021-11-28 21:39:47 -0500

    Improve style

--- src/UI/Hero/Hero.js
+++ src/UI/Hero/Hero.js
@@ -9,8 +9,10 @@ const HeroContainer = styled.section`
   justify-content: center;
   align-items: center;
   padding-top: 60px;
-  background-image: linear-gradient(0deg, rgb(0 0 0 / 45%), rgb(0 0 0 / 42%)),
-    url('https://images.unsplash.com/photo-1583292650898-7d22cd27ca6f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80');
+  /* background-image: linear-gradient(0deg, rgb(0 0 0 / 45%), rgb(0 0 0 / 42%)),
+    url('https://images.unsplash.com/photo-1583292650898-7d22cd27ca6f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80'); */
+  background-image: linear-gradient(177deg, rgb(212 224 222), rgb(0 0 0 / 42%)),
+    url('https://images.unsplash.com/photo-1607768188830-1d6016ae8520?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80');
   background-position: top;
   background-size: cover;
   background-repeat: no-repeat;
@@ -20,18 +22,15 @@ const HeroContainer = styled.section`
     justify-content: flex-start;
     padding-left: 80px;
     background-position: top;
-    color: #000;
   }
 `
 const TextWrapper = styled.div`
   max-width: 400px;
   padding: 0 25px;
   letter-spacing: 1.5px;
-  background-color: rgba(0, 0, 0, 0.25);
-  color: #fff;
+  color: #000;
   @media screen and (min-width: 1023px) {
     padding: 0;
-    background-color: transparent;
   }
 `
 
