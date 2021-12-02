d8f21a561c0540504981e665034eac546b66fddc
Juan Sebastian Mendoza Perez <cebo_052000@hotmail.com>
2021-12-02 12:46:18 -0500

    Add a new folder in which there'll be everyting about the About page

---
+++ src/UI/About/AboutPage/Administradores.js
@@ -0,0 +1,10 @@
+const Administradores = ({ name, info }) => {
+  return (
+    <>
+      <h2>{name}</h2>
+      {/* <img src={picture} href={name} /> */}
+      <p>{info}</p>
+    </>
+  )
+}
+export default Administradores
---
+++ src/UI/About/AboutPage/Information.js
@@ -0,0 +1,17 @@
+const Informacion = () => {
+  return (
+    <>
+      <h2>¿Quiénes Sómos?</h2>
+      <p>
+        LoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLorem
+      </p>
+      <p>
+        LoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLorem
+      </p>
+      <p>
+        LoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLorem
+      </p>
+    </>
+  )
+}
+export default Informacion
