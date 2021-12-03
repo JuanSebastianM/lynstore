50b13df860074c45656169a81cb87bc1fe411d16
Juan Sebastian Mendoza Perez <cebo_052000@hotmail.com>
2021-12-02 21:08:36 -0500

    Update styled component and import components

--- src/pages/Acerca.js
+++ src/pages/Acerca.js
@@ -2,28 +2,51 @@ import styled from 'styled-components'
 import Administradores from '../UI/About/AboutPage/Administradores'
 import Informacion from '../UI/About/AboutPage/Information'
 
-const Container = styled.section`
-  display: grid;
-  grid-template-columns: 1fr 200px 200px;
-  min-height: 100vh;
+const Wrapper = styled.main`
   width: 100%;
-  background-color: red;
+  min-height: 100vh;
+  background-color: #d4e0de;
+`
+
+const Container = styled.section`
+  min-height: 100vh;
+  padding-top: 100px;
+  @media screen and (min-width: 1023px) {
+    display: grid;
+    grid-template-columns: 1fr 400px 400px;
+  }
 `
 
 export const Acerca = () => {
   return (
     <>
-      <Container>
-        <Informacion />
-        <Administradores
-          name='Juan David Mendoza'
-          info='LoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLorem'
-        />
-        <Administradores
-          name='Karolyn Paternina'
-          info='LoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLorem'
-        />
-      </Container>
+      <Wrapper>
+        <Container>
+          <Informacion />
+          <Administradores
+            name='Juan David Mendoza'
+            info={
+              <>
+                <p>Testing testing</p>
+                <p>Testing testing</p>
+                <p>Testing testing</p>
+                <p>Testing testing</p>
+              </>
+            }
+          />
+          <Administradores
+            name='Karolyn Paternina'
+            info={
+              <>
+                <p>Testing testing</p>
+                <p>Testing testing</p>
+                <p>Testing testing</p>
+                <p>Testing testing</p>
+              </>
+            }
+          />
+        </Container>
+      </Wrapper>
     </>
   )
 }
