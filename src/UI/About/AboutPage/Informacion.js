ff5cb09688481f4c86698638aa9b3d277fe86914
Juan Sebastian Mendoza Perez <cebo_052000@hotmail.com>
2021-12-02 21:12:07 -0500

    Create file that contains the info about the store

---
+++ src/UI/About/AboutPage/Informacion.js
@@ -0,0 +1,17 @@
+const Informacion = () => {
+  return (
+    <>
+      <article>
+        <main>
+          <h2>¿Quiénes Sómos?</h2>
+          <div className='text-wrapper'>
+            <p>Lorem lorem lorem lorem lorem lorem lorem lorem</p>
+            <p>Lorem lorem lorem lorem lorem lorem lorem lorem</p>
+            <p>Lorem lorem lorem lorem lorem lorem lorem lorem</p>
+          </div>
+        </main>
+      </article>
+    </>
+  )
+}
+export default Informacion
