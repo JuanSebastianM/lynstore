887c0b80434399e6cac5efc69029203cb93ad0ea
Juan Sebastian Mendoza Perez <cebo_052000@hotmail.com>
2021-12-03 19:49:56 -0500

    Update styles and add Link component from react-router

--- src/UI/Footer/FooterLinks.js
+++ src/UI/Footer/FooterLinks.js
@@ -1,5 +1,7 @@
 // import styled from 'styled-components'
 
+import { Link } from 'react-router-dom'
+
 const FooterLinks = () => {
   return (
     <>
@@ -7,12 +9,12 @@ const FooterLinks = () => {
         <header>
           <h3>Enlaces</h3>
         </header>
-        <div className='centered-col-div'>
-          <a href='/'>Visitar Tienda</a>
-          <a href='/'>¿Quiénes Somos?</a>
-          <a href='/'>Productos</a>
-          <a href='/'>Contacto</a>
-          <a href='/'>Preguntas Frecuentes</a>
+        <div className='links-grid'>
+          <Link to='/'>Visitar Tienda</Link>
+          <Link to='/acerca'>¿Quiénes Somos?</Link>
+          <Link to='/productos'>Productos</Link>
+          <Link to='/contacto'>Contacto</Link>
+          <Link to='/preguntas-frecuentes'>Preguntas Frecuentes</Link>
         </div>
       </div>
     </>
