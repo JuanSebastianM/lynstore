8b7e0c6a06c0ce9eecdc41c092cd4d0abb809fac
Juan Sebastian Mendoza Perez <cebo_052000@hotmail.com>
2021-12-04 19:41:11 -0500

    Remove styled from styled-components and improve the grid behavior from links and improve the whole container's class

--- src/UI/Footer/FooterLinks.js
+++ src/UI/Footer/FooterLinks.js
@@ -1,20 +1,26 @@
-// import styled from 'styled-components'
-
 import { Link } from 'react-router-dom'
 
 const FooterLinks = () => {
   return (
     <>
-      <div className='centered-col-div'>
+      <div className='flex-links'>
         <header>
           <h3>Enlaces</h3>
         </header>
         <div className='links-grid'>
           <Link to='/'>Visitar Tienda</Link>
-          <Link to='/acerca'>¿Quiénes Somos?</Link>
-          <Link to='/productos'>Productos</Link>
-          <Link to='/contacto'>Contacto</Link>
-          <Link to='/preguntas-frecuentes'>Preguntas Frecuentes</Link>
+          <Link to='/acerca' className='desktop-order-3'>
+            ¿Quiénes Somos?
+          </Link>
+          <Link to='/productos' className='desktop-order-5'>
+            Productos
+          </Link>
+          <Link to='/contacto' className='desktop-order-4'>
+            Contacto
+          </Link>
+          <Link to='/preguntas-frecuentes' className='desktop-order-2'>
+            Preguntas Frecuentes
+          </Link>
         </div>
       </div>
     </>
