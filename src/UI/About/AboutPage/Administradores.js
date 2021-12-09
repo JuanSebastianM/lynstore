3a9177dac49cc585e9becae1c981b0175d91491c
Juan Sebastian Mendoza Perez <cebo_052000@hotmail.com>
2021-12-08 20:55:08 -0500

    Improve the UI

--- src/UI/About/AboutPage/Administradores.js
+++ src/UI/About/AboutPage/Administradores.js
@@ -1,14 +1,48 @@
-const Administradores = ({ name, info }) => {
+import styled from 'styled-components'
+import logo from '../../../images/logo.png'
+import { BsSuitHeartFill } from 'react-icons/bs'
+
+const Logo = styled.img`
+  filter: drop-shadow(1px 0 0 #000);
+  max-width: 100%;
+  width: 60px;
+  @media screen and (min-width: 1023px) {
+    width: 100px;
+  }
+`
+const Picture = styled.img`
+  display: block;
+  margin: 0 auto;
+  max-width: 100%;
+  width: 150px;
+  object-fit: contain;
+`
+
+const Administradores = ({ name, picture, info }) => {
   return (
     <>
-      <article>
-        <main>
-          <div className='text-wrapper'>
+      <article className='about-articles'>
+        <div className='text-wrapper'>
+          <header>
             <h2>{name}</h2>
-            {/* <img src={picture} href={name} /> */}
-            {info}
+          </header>
+          <div className='flex-div'>
+            <div className='line'></div>
+            <Logo src={logo} alt='LynStore logo' />
+            <div className='line'></div>
           </div>
-        </main>
+          <Picture src={picture} href={name} />
+          <div className='flex-div'>
+            <div className='line'></div>
+            <Logo src={logo} alt='LynStore logo' />
+            <div className='line'></div>
+          </div>
+          {info}
+          <p>
+            En LynStore tú eres lo más importante{' '}
+            <BsSuitHeartFill className='red-heart' />.
+          </p>
+        </div>
       </article>
     </>
   )
