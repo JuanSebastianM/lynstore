70f864ffabe131c7fad7a7404c606bba59833681
Juan Sebastian Mendoza Perez <cebo_052000@hotmail.com>
2021-12-08 20:59:09 -0500

    Update the content and add style to each piece

--- src/UI/About/AboutPage/Informacion.js
+++ src/UI/About/AboutPage/Informacion.js
@@ -1,16 +1,46 @@
+import styled from 'styled-components'
+
+const Container = styled.article`
+  @media screen and (min-width: 1023px) {
+    max-width: 60%;
+    width: 100%;
+    margin: 0 auto;
+    min-height: 100vh;
+  }
+`
+
 const Informacion = () => {
   return (
     <>
-      <article>
-        <main>
+      <Container className='about-articles'>
+        <header>
           <h2>¿Quiénes Sómos?</h2>
-          <div className='text-wrapper'>
-            <p>Lorem lorem lorem lorem lorem lorem lorem lorem</p>
-            <p>Lorem lorem lorem lorem lorem lorem lorem lorem</p>
-            <p>Lorem lorem lorem lorem lorem lorem lorem lorem</p>
-          </div>
-        </main>
-      </article>
+        </header>
+        <div className='text-wrapper'>
+          <p>
+            <span className='styled-span'>LynStore</span> es un emprendimiento
+            que tuvo sus inicios en el año 2016.
+          </p>
+          <p>
+            En la actualidad estamos dedicados a la comercialización de
+            productos, más especificamente accesorios para mujeres de todas las
+            edades.
+          </p>
+          <p>Además de esto, vendemos a nivel local y nacional.</p>
+          <p>
+            Contamos con una amplia variedad de diseños y colores para todos los
+            gustos. Muchos de estos son fabricados a mano y con diseños
+            originales y exclusivos.
+          </p>
+          <p>
+            Gracias a la buena acogida que ha tenido nuestro negocio,{' '}
+            <span className='styled-span'>LynStore</span> actualmente también
+            cuenta con la posibilidad de vender productos al por mayor y de esta
+            manera ayudar a muchas personas que sueñan con tener su propia
+            empresa y así mismo generar ingresos.
+          </p>
+        </div>
+      </Container>
     </>
   )
 }
