3fda5e598f8bce42f36d602be0712580f7d2b8be
Juan Sebastian Mendoza Perez <cebo_052000@hotmail.com>
2021-12-08 21:08:06 -0500

    Make a new class to style the same span in different places of the app

--- src/UI/Hero/HeroText.js
+++ src/UI/Hero/HeroText.js
@@ -1,19 +1,5 @@
 import styled from 'styled-components/macro'
 
-const StyledText = styled.span`
-color: #d4e0d3;
-text-shadow: 2px 1px 0px #54adb5;
-  background-image: linear-gradient(120deg, #000 0%, #000 100%);
-  background-repeat: no-repeat;
-  background-size: 100% 0.2em;
-  background-position: 0% 88%;
-  transition: all 500ms ease;
-  &:hover {
-    text-shadow: none;
-    background-size: 100% 88%;
-  }
-  border-radius: 50%;
-`
 const Paragraph = styled.p`
   margin-bottom: 15px;
 `
@@ -30,7 +16,7 @@ const HeroText = () => {
       </Paragraph>
       <Paragraph>Sé feliz como TÚ QUIERAS.</Paragraph>
       <Paragraph>
-        Sé feliz con <StyledText>LynStore</StyledText>.
+        Sé feliz con <span className='styled-span'>LynStore</span>.
       </Paragraph>
     </>
   )
