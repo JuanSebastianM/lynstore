39a50275196de11edbf22348ca039a19ff5ad14e
Juan Sebastian Mendoza Perez <cebo_052000@hotmail.com>
2021-12-08 21:04:55 -0500

    Import images of the products and improve style

--- src/UI/Products/ProductsContent.js
+++ src/UI/Products/ProductsContent.js
@@ -1,20 +1,19 @@
 import styled from 'styled-components/macro'
 import ProductPreview from './ProductPreview'
-import {
-  GiHeartEarrings,
-  GiHeartNecklace,
-  GiBelt,
-  GiSunglasses,
-} from 'react-icons/gi'
+
+import aretes from '../../images/aretes.jpg'
+import anillo from '../../images/anillo.jpg'
+import cadena from '../../images/cadena.jpg'
+import pulseras from '../../images/pulseras.jpg'
+import sujetador from '../../images/sujetador.jpg'
 
 const Wrapper = styled.div`
-@media screen and (min-width: 1023px) {
-
+  @media screen and (min-width: 1023px) {
     display: grid;
     grid-gap: 20px;
     justify-content: center;
     grid-template-columns: repeat(3, 300px);
-}
+  }
 `
 
 const ProductsContent = () => {
@@ -22,26 +21,11 @@ const ProductsContent = () => {
     <>
       <h2>Productos</h2>
       <Wrapper>
-        <ProductPreview
-          title='Aretes'
-          image={<GiHeartEarrings className='product-img' />}
-          text='Prueba prueba prueba'
-        />
-        <ProductPreview
-          title='Cadenas'
-          image={<GiHeartNecklace className='product-img' />}
-          text='Prueba prueba prueba'
-        />
-        <ProductPreview
-          title='Pulseras'
-          image={<GiBelt className='product-img' />}
-          text='Prueba prueba prueba'
-        />
-        <ProductPreview
-          title='Sujetador de gafas'
-          image={<GiSunglasses className='product-img' />}
-          text='Prueba prueba prueba'
-        />
+        <ProductPreview title='Aretes' image={aretes} link='/' />
+        <ProductPreview title='Anillos' image={anillo} link='/' />
+        <ProductPreview title='Cadenas' image={cadena} link='/' />
+        <ProductPreview title='Pulseras' image={pulseras} link='/' />
+        <ProductPreview title='Sujetador de gafas' image={sujetador} link='/' />
       </Wrapper>
     </>
   )
