474ffba052f4097f49f128f677747e136a1b92d2
Juan Sebastian Mendoza Perez <cebo_052000@hotmail.com>
2021-12-08 20:51:10 -0500

    Update styles and improve the semantics

--- src/pages/Acerca.js
+++ src/pages/Acerca.js
@@ -5,19 +5,15 @@ import juand from '../images/juand.jpg'
 import karolyn from '../images/karolyn.jpg'
 
 const Wrapper = styled.main`
+  padding: 100px 0 30px;
   width: 100%;
   min-height: 100vh;
-  background-color: #d4e0de;
 `
-const Container = styled.section`
-  min-height: 100vh;
-  padding-top: 100px;
-  @media screen and (max-width: 1023px) {
-    padding-bottom: 30px;
-  }
+const AdminsWrapper = styled.div`
+  margin-top: 30px;
   @media screen and (min-width: 1023px) {
-    display: grid;
-    grid-template-columns: 1fr 400px 400px;
+    display: flex;
+    justify-content: space-evenly;
   }
 `
 
@@ -25,45 +21,50 @@ export const Acerca = () => {
   return (
     <>
       <Wrapper>
-        <Container>
-          <Informacion />
-          <Administradores
-            name='Juan David Mendoza'
-            picture={juand}
-            info={
-              <>
-                <p>Soy una de las dos manos creativas de LynStore.</p>
-                <p>
-                  Me considero un hombre inteligente, divertido, temeroso de
-                  Dios y paciente.
-                </p>
-                <p>
-                  Considero que lo hecho a mano le da un plus a nuestros
-                  productos, porque además de hacerlo con mucha dedicación,
-                  podemos personalizarlos al gusto de cliente.
-                </p>
-              </>
-            }
-          />
-          <Administradores
-            name='Karolyn Paternina'
-            picture={karolyn}
-            info={
-              <>
-                <p>También soy una de las dos manos creativas de LynStore.</p>
-                <p>
-                  Me considero una mujer inteligente, creativa, soñadora y llena
-                  del amor de Dios.
-                </p>
-                <p>
-                  Amo diseñar tus accesorios y luego materializarlos con mis
-                  manos, porque es sinónimo de que tu producto es 100% original
-                  y hecho con mucho amor.
-                </p>
-              </>
-            }
-          />
-        </Container>
+        <Informacion />
+        <section>
+          <header>
+            <h2>Nuestro Equipo</h2>
+          </header>
+          <AdminsWrapper>
+            <Administradores
+              name='Juan David Mendoza'
+              picture={juand}
+              info={
+                <>
+                  <p>Soy una de las dos manos creativas de LynStore.</p>
+                  <p>
+                    Me considero un hombre inteligente, divertido, temeroso de
+                    Dios y paciente.
+                  </p>
+                  <p>
+                    Considero que lo hecho a mano le da un plus a nuestros
+                    productos, porque además de hacerlo con mucha dedicación,
+                    podemos personalizarlos al gusto de cliente.
+                  </p>
+                </>
+              }
+            />
+            <Administradores
+              name='Karolyn Paternina'
+              picture={karolyn}
+              info={
+                <>
+                  <p>También soy una de las dos manos creativas de LynStore.</p>
+                  <p>
+                    Me considero una mujer inteligente, creativa, soñadora y
+                    llena del amor de Dios.
+                  </p>
+                  <p>
+                    Amo diseñar tus accesorios y luego materializarlos con mis
+                    manos, porque es sinónimo de que tu producto es 100%
+                    original y hecho con mucho amor.
+                  </p>
+                </>
+              }
+            />
+          </AdminsWrapper>
+        </section>
       </Wrapper>
     </>
   )
