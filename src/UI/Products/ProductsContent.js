1843ec321256b0445ef5222418092b8275a60230
Juan Sebastian Mendoza Perez <cebo_052000@hotmail.com>
2021-12-10 19:37:21 -0500

    Improve the style both for mobile and desktop viewports

--- src/UI/Products/ProductsContent.js
+++ src/UI/Products/ProductsContent.js
@@ -1,20 +1,28 @@
-import styled from 'styled-components/macro'
-import ProductPreview from './ProductPreview'
+import styled from 'styled-components/macro';
+import ProductPreview from './ProductPreview';
 
-import aretes from '../../images/aretes.jpg'
-import anillo from '../../images/anillo.jpg'
-import cadena from '../../images/cadena.jpg'
-import pulseras from '../../images/pulseras.jpg'
-import sujetador from '../../images/sujetador.jpg'
+import aretes from '../../images/aretes.jpg';
+import anillo from '../../images/anillo.jpg';
+import cadena from '../../images/cadena.jpg';
+import pulseras from '../../images/pulseras.jpg';
+import sujetador from '../../images/sujetador.jpg';
 
 const Wrapper = styled.div`
+  display: grid;
+  padding: 40px 0 30px;
+  grid-gap: 40px;
+  grid-template-columns: 210px;
+  justify-content: center;
+  @media screen and (min-width: 767px) {
+    grid-template-columns: 250px 250px;
+    grid-gap: 60px;
+  }
   @media screen and (min-width: 1023px) {
-    display: grid;
-    grid-gap: 20px;
-    justify-content: center;
+    padding: 40px 0 140px;
+    padding-top: 50px;
     grid-template-columns: repeat(3, 300px);
   }
-`
+`;
 
 const ProductsContent = () => {
   return (
@@ -28,6 +36,6 @@ const ProductsContent = () => {
         <ProductPreview title='Sujetador de gafas' image={sujetador} link='/' />
       </Wrapper>
     </>
-  )
-}
-export default ProductsContent
+  );
+};
+export default ProductsContent;
