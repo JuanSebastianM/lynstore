9f2b6dab074217d94cd1a28b4b304aec631a08de
Juan Sebastian Mendoza Perez <cebo_052000@hotmail.com>
2021-12-10 19:38:18 -0500

    Improve the style and add an SVG to the background design

--- src/pages/Productos.js
+++ src/pages/Productos.js
@@ -1,12 +1,11 @@
-import styled from 'styled-components'
-import fondo2 from '../images/fondo2.jpg'
+import styled from 'styled-components';
 
-import ProductsContent from '../UI/Products/ProductsContent'
+import ProductsContent from '../UI/Products/ProductsContent';
 
 const Container = styled.section`
-  /* background-image: url('https://images.unsplash.com/photo-1558882268-15aa056d885f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80');
+  background-image: url('https://res.cloudinary.com/dmhhz7iam/image/upload/v1639165525/Twinkle_Star-0.7s-1366px_nkysbw.png');
   background-size: cover;
-  background-repeat: no-repeat; */
+  background-repeat: no-repeat;
   position: relative;
   width: 100%;
   padding-top: 100px;
@@ -14,24 +13,24 @@ const Container = styled.section`
     padding-bottom: 30px;
   }
   min-height: 100vh;
-`
-const BackgroundImage = styled.img`
-  position: absolute;
-  z-index: -6;
-  top: 0;
-  right: 0;
-  width: 100%;
-  height: 100%;
-  object-fit: contain;
-`
-
+`;
 export const Productos = () => {
   return (
     <>
       <Container>
-        <BackgroundImage src={fondo2} alt='Dos aretes y una vincha blanca' />
         <ProductsContent />
+        <svg
+          className='styled-svg'
+          xmlns='http://www.w3.org/2000/svg'
+          viewBox='0 0 1440 320'
+        >
+          <path
+            fill='#faebd7'
+            fillOpacity='1'
+            d='M0,160L48,181.3C96,203,192,245,288,256C384,267,480,245,576,229.3C672,213,768,203,864,208C960,213,1056,235,1152,202.7C1248,171,1344,85,1392,42.7L1440,0L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z'
+          ></path>
+        </svg>
       </Container>
     </>
-  )
-}
+  );
+};
