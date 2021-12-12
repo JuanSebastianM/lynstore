513fce32ba46d82e21bbf165800f54ab532e094a
Juan Sebastian Mendoza Perez <cebo_052000@hotmail.com>
2021-12-12 15:38:18 -0500

    Create a new file that contains the submit button

---
+++ src/UI/Contact/ContactSubmit.js
@@ -0,0 +1,27 @@
+import styled from 'styled-components';
+
+const SubmitBtn = styled.button`
+  padding: 10px 0;
+  cursor: pointer;
+  width: 100%;
+  max-width: 300px;
+  margin-top: 20px;
+  border-radius: 10px;
+  border: 1px solid #000;
+  background-color: #000;
+  color: #ffd3d3;
+  transition: all 500ms ease;
+  &:hover {
+    background-color: #fff;
+    color: #000;
+  }
+`;
+
+const ContactSubmit = () => {
+  return (
+    <>
+      <SubmitBtn type='submit'>Enviar</SubmitBtn>
+    </>
+  );
+};
+export default ContactSubmit;
