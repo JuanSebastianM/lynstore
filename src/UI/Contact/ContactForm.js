f1e84f461cc2fa391f366b258dee5e31595ddca2
Juan Sebastian Mendoza Perez <cebo_052000@hotmail.com>
2021-12-12 15:39:10 -0500

    Add more inputs and relevant items

--- src/UI/Contact/ContactForm.js
+++ src/UI/Contact/ContactForm.js
@@ -1,5 +1,7 @@
 import styled from 'styled-components';
 
+import ContactSubmit from './ContactSubmit';
+
 const FormContainer = styled.div`
   padding: 20px;
   border: 2px solid #000;
@@ -13,14 +15,50 @@ const ContactForm = () => {
         <form className='styled-form'>
           <div className='form-names'>
             <div>
-              <label htmlFor='firstName'>Primer Nombre</label>
-              <input type='text' id='firstName' required />
+              <label htmlFor='firstName' className='required'>
+                Primer Nombre
+              </label>
+              <input
+                type='text'
+                id='firstName'
+                name='firstName'
+                placeholder='Susana'
+                required
+              />
             </div>
             <div>
               <label htmlFor='lastName'>Primer Apellido</label>
-              <input type='text' id='lastName' required />
+              <input
+                type='text'
+                id='lastName'
+                name='lastName'
+                placeholder='Rodríguez'
+              />
             </div>
           </div>
+          <label htmlFor='email' className='required'>
+            Correo Electrónico
+          </label>
+          <input
+            type='email'
+            id='email'
+            name='email'
+            placeholder='tucorreo@ejemplo.com'
+            required
+          />
+          <label htmlFor='message' className='required'>
+            Mensaje
+          </label>
+          <textarea
+            id='message'
+            name='message'
+            placeholder='Escribe tu mensaje aquí...'
+            required
+          />
+          <p className='form-paragraph'>
+            Los campos con un <span>*</span> son obligatorios.
+          </p>
+          <ContactSubmit />
         </form>
       </FormContainer>
     </>
