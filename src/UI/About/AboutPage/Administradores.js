444ff12dc77f6b737bf2d3dce9e524eb69198b9d
Juan Sebastian Mendoza Perez <cebo_052000@hotmail.com>
2021-12-12 10:40:12 -0500

    Improve the styles

--- src/UI/About/AboutPage/Administradores.js
+++ src/UI/About/AboutPage/Administradores.js
@@ -1,6 +1,6 @@
-import styled from 'styled-components'
-import logo from '../../../images/logo.png'
-import { BsSuitHeartFill } from 'react-icons/bs'
+import styled from 'styled-components';
+import logo from '../../../images/logo.png';
+import { BsSuitHeartFill } from 'react-icons/bs';
 
 const Logo = styled.img`
   filter: drop-shadow(1px 0 0 #000);
@@ -9,20 +9,20 @@ const Logo = styled.img`
   @media screen and (min-width: 1023px) {
     width: 100px;
   }
-`
+`;
 const Picture = styled.img`
   display: block;
+  border: 10px solid #ffe6ed;
   margin: 0 auto;
   max-width: 100%;
   width: 150px;
   object-fit: contain;
-`
+`;
 
 const Administradores = ({ name, picture, info }) => {
   return (
     <>
       <article className='about-articles'>
-        <div className='text-wrapper'>
           <header>
             <h2>{name}</h2>
           </header>
@@ -31,20 +31,21 @@ const Administradores = ({ name, picture, info }) => {
             <Logo src={logo} alt='LynStore logo' />
             <div className='line'></div>
           </div>
-          <Picture src={picture} href={name} />
+          <Picture src={picture} alt={name} />
           <div className='flex-div'>
             <div className='line'></div>
             <Logo src={logo} alt='LynStore logo' />
             <div className='line'></div>
           </div>
-          {info}
-          <p>
-            En LynStore tú eres lo más importante{' '}
-            <BsSuitHeartFill className='red-heart' />.
-          </p>
-        </div>
+          <div className='text-wrapper'>
+            {info}
+            <p>
+              En LynStore tú eres lo más importante{' '}
+              <BsSuitHeartFill className='red-heart' />.
+            </p>
+          </div>
       </article>
     </>
-  )
-}
-export default Administradores
+  );
+};
+export default Administradores;
