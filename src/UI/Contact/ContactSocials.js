9d82b363ae1cbac35173f4b3993f7c60343e14be
Juan Sebastian Mendoza Perez <cebo_052000@hotmail.com>
2021-12-14 20:57:32 -0500

    Import icons and create a styled element

--- src/UI/Contact/ContactSocials.js
+++ src/UI/Contact/ContactSocials.js
@@ -1,19 +1,32 @@
+import styled from 'styled-components';
+
+import { BsWhatsapp, BsInstagram } from 'react-icons/bs';
+
+const List = styled.li`
+  display: grid;
+  grid-template-columns: 30px 1fr;
+  text-align: left;
+  gap: 10px;
+`;
+
 const ContactSocials = () => {
   return (
     <>
-      <div>
+      <div className='socials'>
         <h3>Redes Sociales</h3>
         <ul>
-          <li>
+          <List>
+            <BsWhatsapp />
             <a href='/' target='_blank' rel='noopener noreferrer'>
               WhatsApp
             </a>
-          </li>
-          <li>
+          </List>
+          <List>
+            <BsInstagram />
             <a href='/' target='_blank' rel='noopener noreferrer'>
               Instagram
             </a>
-          </li>
+          </List>
         </ul>
       </div>
     </>
