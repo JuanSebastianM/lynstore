8eb1b0bd1f80e3005dd1b83d46eb1219bfbc2e3c
Juan Sebastian Mendoza Perez <cebo_052000@hotmail.com>
2021-12-14 20:55:47 -0500

    Remove min-height from image section

--- src/UI/About/AboutImage.js
+++ src/UI/About/AboutImage.js
@@ -1,7 +1,7 @@
-import styled, { keyframes } from 'styled-components/macro'
-import office from '../../images/office.jpg'
-import calma from '../../images/calma.png'
-import dosalmas from '../../images/dosalmas.png'
+import styled, { keyframes } from 'styled-components/macro';
+import office from '../../images/office.jpg';
+import calma from '../../images/calma.png';
+import dosalmas from '../../images/dosalmas.png';
 
 const breezeOne = keyframes`
 0% {
@@ -13,7 +13,7 @@ const breezeOne = keyframes`
 100% {
     transform: rotate(15deg);
 }
-`
+`;
 const breezeTwo = keyframes`
 0% {
     transform: rotate(-15deg);
@@ -24,18 +24,20 @@ const breezeTwo = keyframes`
 100% {
     transform: rotate(-15deg);
 }
-`
+`;
 const ImageContainer = styled.div`
-  min-height: 100vh;
+  @media screen and (max-width: 1023px) {
+    padding-bottom: 40px;
+  }
   @media screen and (min-width: 1023px) {
     position: relative;
   }
-`
+`;
 const SiteImage = styled.img`
   object-fit: contain;
   width: 100%;
   z-index: -1;
-`
+`;
 const ImagesContainer = styled.div`
   display: flex;
   height: 140px;
@@ -48,7 +50,7 @@ const ImagesContainer = styled.div`
     height: 100%;
     justify-content: space-evenly;
   }
-`
+`;
 const ImageOneContainer = styled.div`
   box-shadow: 4px 4px 5px -4px #000;
   border-radius: 3px;
@@ -85,24 +87,24 @@ const ImageOneContainer = styled.div`
       height: 40px;
     }
   }
-`
+`;
 const ImageCardOne = styled(SiteImage)`
   transform: rotate(10deg);
   filter: drop-shadow(0px 1px 6px #fff);
   @media screen and (min-width: 1023px) {
     width: 190px;
   }
-`
+`;
 const ImageTwoContainer = styled(ImageOneContainer)`
   transform: rotate(-15deg);
   align-self: flex-start;
   &:hover {
     animation-name: ${breezeTwo};
   }
-`
+`;
 const ImageCardTwo = styled(ImageCardOne)`
   transform: rotate(325deg);
-`
+`;
 
 const AboutImage = () => {
   return (
@@ -117,6 +119,6 @@ const AboutImage = () => {
         </ImageTwoContainer>
       </ImagesContainer>
     </ImageContainer>
-  )
-}
-export default AboutImage
+  );
+};
+export default AboutImage;
