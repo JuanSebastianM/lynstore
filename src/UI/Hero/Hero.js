efa0378802fa444ed09af45d0d833ccebcc79cf3
Juan Sebastian Mendoza Perez <cebo_052000@hotmail.com>
2021-12-18 17:49:12 -0500

    Import the new component

--- src/UI/Hero/Hero.js
+++ src/UI/Hero/Hero.js
@@ -1,14 +1,18 @@
-import styled from 'styled-components'
-import HeroLinks from './HeroLinks'
-import HeroText from './HeroText'
+import styled from 'styled-components';
+import HeroLinks from './HeroLinks';
+import HeroSmartphone from './HeroSmartphone';
+import HeroText from './HeroText';
 
 const HeroContainer = styled.section`
   width: 100%;
   min-height: 100vh;
   display: flex;
+  @media screen and (max-width: 767px) {
+    flex-direction: column;
+  }
   justify-content: center;
   align-items: center;
-  padding-top: 60px;
+  padding-top: 100px;
   /* background-image: linear-gradient(0deg, rgb(0 0 0 / 45%), rgb(0 0 0 / 42%)),
     url('https://images.unsplash.com/photo-1583292650898-7d22cd27ca6f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80'); */
   background-image: linear-gradient(177deg, rgb(212 224 222), rgb(0 0 0 / 42%)),
@@ -20,19 +24,19 @@ const HeroContainer = styled.section`
   background-attachment: fixed;
   @media screen and (min-width: 1023px) {
     justify-content: flex-start;
-    padding-left: 80px;
+    padding: 0 110px;
     background-position: top;
   }
-`
+`;
 const TextWrapper = styled.div`
   max-width: 400px;
-  padding: 0 25px;
+  padding: 0 40px;
   letter-spacing: 1.5px;
   color: #000;
   @media screen and (min-width: 1023px) {
     padding: 0;
   }
-`
+`;
 
 const Hero = () => {
   return (
@@ -41,7 +45,8 @@ const Hero = () => {
         <HeroText />
         <HeroLinks />
       </TextWrapper>
+      <HeroSmartphone />
     </HeroContainer>
-  )
-}
-export default Hero
+  );
+};
+export default Hero;
