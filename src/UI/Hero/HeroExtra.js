e96e34e559048f6d2912c0a8e676357747bba81c
Juan Sebastian Mendoza Perez <cebo_052000@hotmail.com>
2021-12-19 18:16:47 -0500

    Add a new component for the Hero section

---
+++ src/UI/Hero/HeroExtra.js
@@ -0,0 +1,15 @@
+const HeroExtra = () => {
+  return (
+    <>
+      <header>
+        <h2>¡SOMOS MAYORISTAS!</h2>
+      </header>
+      <p>¡Invierte con nosotros y obtén hasta el 50% en gananacias!</p>
+      <p>
+        Además, puedes obtener grandiosos beneficios por distribuir nuestros
+        accesorios.
+      </p>
+    </>
+  );
+};
+export default HeroExtra;
