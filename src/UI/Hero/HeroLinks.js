f9041184ee04c1b757633c8e2febda6c9035e4e7
Juan Sebastian Mendoza Perez <cebo_052000@hotmail.com>
2021-12-19 18:24:41 -0500

    Remove import of styled from styled-components and change the content so that it now displays either a Link from react-router or a normal anchor tag depending on the value of linkRouter prop

--- src/UI/Hero/HeroLinks.js
+++ src/UI/Hero/HeroLinks.js
@@ -1,37 +1,18 @@
-import styled from 'styled-components'
+import { Link } from 'react-router-dom';
 
-const LinkBtn = styled.a`
-  display: block;
-  text-align: center;
-  padding: 5px 10px;
-  border-radius: 10px;
-  background-color: #000;
-  color: #d4e0de;
-  cursor: pointer;
-  border: 2px solid transparent;
-  transform: scale(1);
-  transition: all 500ms ease;
-  &:hover {
-    color: #000;
-    background-color: transparent;
-    border: 2px solid #000;
-  }
-  &:active {
-    transform: scale(0.8);
-  }
-`
-const Wrapper = styled.div`
-  display: flex;
-  justify-content: space-between;
-`
-
-const HeroLinks = () => {
+const HeroLinks = ({ linkRouter, link, text }) => {
   return (
     <>
-      <Wrapper>
-        <LinkBtn>Comprar</LinkBtn>
-      </Wrapper>
+      {linkRouter ? (
+        <Link className='hero-link' to={link}>
+          {text}
+        </Link>
+      ) : (
+        <a className='hero-link' href={link}>
+          {text}
+        </a>
+      )}
     </>
-  )
-}
-export default HeroLinks
+  );
+};
+export default HeroLinks;
