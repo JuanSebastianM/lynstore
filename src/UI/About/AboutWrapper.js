acf59bc8adb85185a28c0465baef112be3848b4a
Juan Sebastian Mendoza Perez <cebo_052000@hotmail.com>
2021-12-19 18:20:35 -0500

    Remove min-height CSS property

--- src/UI/About/AboutWrapper.js
+++ src/UI/About/AboutWrapper.js
@@ -1,14 +1,13 @@
-import styled from 'styled-components'
+import styled from 'styled-components';
 
 const Wrapper = styled.section`
-  min-height: 100vh;
   @media screen and (min-width: 1023px) {
     display: grid;
     grid-template-columns: 1fr 1fr;
     align-items: center;
   }
-`
+`;
 const AboutWrapper = (props) => {
-  return <Wrapper>{props.children}</Wrapper>
-}
-export default AboutWrapper
+  return <Wrapper>{props.children}</Wrapper>;
+};
+export default AboutWrapper;
