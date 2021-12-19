4f559e1b764e003ddafdf5ce1e332df44eeb45e5
Juan Sebastian Mendoza Perez <cebo_052000@hotmail.com>
2021-12-19 18:26:58 -0500

    Update imports and Wrapper styled component's style

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,13 @@
-import React from 'react'
-import styled from 'styled-components/macro'
+import styled from 'styled-components/macro';
 
-import Hero from '../UI/Hero/Hero'
-import About from '../UI/About/About'
+import Hero from '../UI/Hero/Hero';
+import About from '../UI/About/About';
 
 const Wrapper = styled.main`
   width: 100%;
   min-height: 100vh;
-  background-color: #d4e0de;
-`
+  background-color: #fff;
+`;
 
 export const Home = () => {
   return (
@@ -18,5 +17,5 @@ export const Home = () => {
         <About />
       </Wrapper>
     </>
-  )
-}
+  );
+};
