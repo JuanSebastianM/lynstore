7ca82966240a7b7c22dab8c5caf9e9802d4fe65e
Juan Sebastian Mendoza <cebo_052000@hotmail.com>
2021-12-21 19:09:16 -0500

    Update functions/mailer.js reference in function

--- functions/mailer.js
+++ functions/mailer.js
@@ -13,6 +13,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
+app.use('/.netlify/functions/mailer', router);
 
 router.post('/enviar', (req, res) => {
   console.log(req.body.formData);
@@ -74,7 +75,6 @@ router.post('/enviar', (req, res) => {
       console.log('Error en el envío!', error.message);
     });
 });
-app.use('/.netlify/functions/server', router);
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
